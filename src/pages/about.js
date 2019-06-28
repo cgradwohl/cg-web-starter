8ae47accbce621c07ba95ec052d7c5cd51c9cbb5
Chris Gradwohl <christophergradwohl@gmail.com>
2019-06-27 21:49:55 -0700

    adds font awesome webfonts

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,20 +4,29 @@ import React from 'react';
 const About = () => (
   <>
     <div className="columns">
-      <div className="column">1</div>
-      <div className="column">2</div>
-      <div className="column">3</div>
-      <div className="column">1</div>
-      <div className="column">1</div>
-      <div className="column">1</div>
-      <div className="column">4</div>
+      <div className="column">
+        <i className="fas fa-fish" />
+      </div>
+      <div className="column">
+        <i className="fas fa-fish" />
+      </div>
+      <div className="column">
+        <i className="fas fa-frog" />
+      </div>
+      <div className="column">
+        <i className="fas fa-user-ninja vanished" />
+      </div>
+      <div className="column">
+        <i className="fas fa-user-ninja vanished" />
+      </div>
+      <div className="column">
+        <i className="fas fa-user-ninja vanished" />
+      </div>
+      <div className="column">
+        <i className="fab fa-facebook" />
+      </div>
 
       <button className="button is-large is-success" type="button">Button</button>
-
-      <i className="fas fa-fish" />
-      <i className="fas fa-frog" />
-      <i className="fas fa-user-ninja vanished" />
-      <i className="fab fa-facebook" />
     </div>
   </>
 );
